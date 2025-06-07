ee6eb9914031ded06cd76fa38a53435bcb212ca9
StefanMogren <stefan_mogren@hotmail.com>
2025-06-07 14:33:57 +0200

    Replaced unused API-calls in menu.controller.js with new ones

--- controllers/menu.controller.js
+++ controllers/menu.controller.js
@@ -11,71 +11,40 @@ export const getAllMenuItems = async (req, res, next) => {
 		next(new CustomError("Failed to get menu items", 500, err));
 	}
 };
+
+// POST /api/menu
 /* 
-// POST /api/menu/seed
-export const seedMenu = async (req, res, next) => {
-	try {
-		const mockItems = [
-			{
-				prodId: "prod-jespe",
-				title: "Bryggkaffe",
-				desc: "Bryggd på månadens bönor.",
-				price: 39,
-			},
-			{
-				prodId: "prod-rnybe",
-				title: "Caffè Doppio",
-				desc: "Bryggd på månadens bönor.",
-				price: 49,
-			},
-			{
-				prodId: "prod-rgist",
-				title: "Cappuccino",
-				desc: "Bryggd på månadens bönor.",
-				price: 49,
-			},
-			{
-				prodId: "prod-hebes",
-				title: "Latte Macchiato",
-				desc: "Bryggd på månadens bönor.",
-				price: 49,
-			},
-			{
-				prodId: "prod-tofth",
-				title: "Kaffe Latte",
-				desc: "Bryggd på månadens bönor.",
-				price: 54,
-			},
-			{
-				prodId: "prod-ebest",
-				title: "Cortado",
-				desc: "Bryggd på månadens bönor.",
-				price: 39,
-			},
-		];
+Description: Adds new product to menu
 
-		await MenuItem.deleteMany();
-		const inserted = await MenuItem.insertMany(mockItems);
-		res.status(201).json(inserted);
-	} catch (err) {
-		next(new CustomError("Failed to seed menu items", 500, err));
-	}
-};
- */
+Headers: Authorization : <token>
+
+Body:
+
+{
+  "title" : <product name>,
+  "desc" : <product description>,
+  "price" : <product price>
+} 
+*/
+
+// PUT /api/menu/{prodId}
 /* 
-// POST /api/menu  // Adding a new item/items
-export const addMenuItem = async (req, res, next) => {
-	const { prodId, title, desc, price } = req.body;
+Description: Updates product in menu
 
-	if (!prodId || !title || !desc || typeof price !== "number") {
-		throw new CustomError("Invalid menu item data", 400);
-	}
+Headers: Authorization : <token>
 
-	try {
-		const newItem = await MenuItem.create(req.body);
-		res.status(201).json(newItem);
-	} catch (err) {
-		next(new CustomError("Failed to add menu item", 400, err));
-	}
-};
- */
+Body:
+
+{
+  "title" : <product name>,
+  "desc" : <product description>,
+  "price" : <product price>
+}
+*/
+
+// DELETE /api/menu/{prodId}
+/* 
+Description: Deletes product in menu
+
+Headers: Authorization : <token>
+*/
